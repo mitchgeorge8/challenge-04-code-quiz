bd086d945173460b511cc978e9c4d70ef518e0ad
Mitchell George <mitchgeorge8@gmail.com>
2022-03-05 23:13:49 -0500

    Improved usability. Added timer and first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,12 @@
 // Each question has 4 multiple choice options, and each option is a button
 // Once all questions have been answered, the timer stops and displays time as score
 
+var timerEl = document.querySelector("#timer");
 var pageContentEl = document.querySelector("#page-content");
 var quizStartEl = document.querySelector("#quiz-start");
 var quizQuestionEl = document.querySelector("#quiz-question");
+
+var startTime = 75;
 
 var questionsArr = [
     "Commonly used data types DO NOT include:",
@@ -24,19 +27,57 @@ var answersArr = [
     "JavaScript","Terminal/Bash","For loops","Console.log"
 ];
 
+var correctAnswersArr = [
+    "option3",
+    "option3",
+    "option4",
+    "option3",
+    "option4"
+]
+
 var buttonHandler = function(event) {
     var targetEl = event.target;
 
     console.log(targetEl);
 
-    if (targetEl.matches("#start-quiz")) {
-        runQuiz(0);
+    switch(true) {
+        case targetEl.matches("#start-quiz"):
+            // Hide quizStartEl at the beginning of the quiz
+            quizStartEl.style.display = "none";
+
+            // Start the quiz
+            runQuiz(0);
+
+            break;
+        case targetEl.matches(".btn-answer"):
+            checkAnswer()
+
+            break;
+    }
+    
+}
+
+function runQuiz(questionId) {
+    if (questionId < questionsArr.length) {
+
+        timeCountDown();
+        var quizTime = setInterval(timeCountDown, 1000);
+
+        createQuizQuestion(questionId);
+    }
+    else {
+        clearInterval(quizTime);
+        var quizScore = startTime;
+        console.log(quizScore);
     }
 }
 
-var runQuiz = function(questionId) {
-    quizStartEl.style.display = "none";
+function timeCountDown() {
+    timerEl.textContent = "Time: " + startTime;
+    startTime--;
+}
 
+function createQuizQuestion(questionId) {
     var questionEl = document.createElement("h1");
     questionEl.innerHTML = questionsArr[questionId];
     quizQuestionEl.appendChild(questionEl);
@@ -48,10 +89,10 @@ var runQuiz = function(questionId) {
     var listItemEl3 = document.createElement("li");
     var listItemEl4 = document.createElement("li");
 
-    listItemEl1.innerHTML = answersArr[questionId];
-    listItemEl2.innerHTML = answersArr[questionId + 1];
-    listItemEl3.innerHTML = answersArr[questionId + 2];
-    listItemEl4.innerHTML = answersArr[questionId + 3];
+    listItemEl1.innerHTML = "<button class='btn btn-answer' id='option1'>1. " + answersArr[questionId] + "</button>";
+    listItemEl2.innerHTML = "<button class='btn btn-answer' id='option2'>2. " + answersArr[questionId + 1] + "</button>";
+    listItemEl3.innerHTML = "<button class='btn btn-answer' id='option3'>3. " + answersArr[questionId + 2] + "</button>";
+    listItemEl4.innerHTML = "<button class='btn btn-answer' id='option4'>4. " + answersArr[questionId + 3] + "</button>";
 
     answersEl.appendChild(listItemEl1);
     answersEl.appendChild(listItemEl2);
